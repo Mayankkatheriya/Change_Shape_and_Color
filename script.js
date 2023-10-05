52ecd0013a37a67e7a9f06842dcd6aea193c40e9
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-10-06 02:33:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let outerDiv = document.querySelector("#outer");
+let innerDiv = document.querySelector("#inner");
+let colorButton = document.querySelector("#colorChange");
+let shapeButton = document.querySelector("#shapeChange");
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+//?random hex code of color
+// const hexCharacters = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+// function getCharacter(index) {
+// 	return hexCharacters[index]
+// }
+
+// function generateNewColor() {
+// 	let hexColorRep = "#"
+
+// 	for (let index = 0; index < 6; index++){
+// 		const randomPosition = Math.floor ( Math.random() * hexCharacters.length ) 
+//     	hexColorRep += getCharacter( randomPosition )
+// 	}
+	
+// 	return hexColorRep
+// }
+
+//? random rgb color code
+function randomRGB() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var RGBColor = "rgb(" + x + "," + y + "," + z + ")";  
+    return RGBColor;
+  }
+colorButton.addEventListener("click", ()=>{
+    audio.play();
+    let newColor = randomRGB();
+    outerDiv.style.backgroundColor = newColor;
+    outerDiv.style.transition = "0.8s ease-in-out";
+
+})
+
+//change shape randomly
+let shapes = ["inner", "diamond", "triangle", "circle", "rabbet", "rectangle", "cross", "frame", "leftArrow", "rightPoint"];
+function shapeSelect() {
+    const randomid = Math.floor ( Math.random() * shapes.length);
+    return shapes[randomid];
+}
+shapeButton.addEventListener("click", ()=>{
+    audio.play();
+    let idname = shapeSelect();
+    innerDiv.id = idname;
+    innerDiv.style.transition = "0.8s ease-in-out";
+})
